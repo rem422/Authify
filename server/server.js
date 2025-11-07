c721fe0c5700480e6a361045374bdc8c95650c5f
Rem Simiyu <remsimiyu@gmail.com>
2025-11-07 16:30:34 +0300

    Create a test API route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRoutes  from './router/user.route.js'
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const DB = process.env.MONGODB_URI;
 
 const app = express()
+
+app.use("/api/user", userRoutes)
+
 
 const conectDB = async() => {
     try {
