6d5ffa6e5cec1b6b8c7f12ec267f9038e76ca746
Rem Simiyu <remsimiyu@gmail.com>
2025-11-07 14:47:37 +0300

    Installing react js and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-blue-500 text-4xl bg-blue-950'>Hello There</h1>
+    </div>
+  )
+}
+
+export default App 
