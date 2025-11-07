a05ef6db777f63f18e6ef898c5d3bcb557cd7709
Rem Simiyu <remsimiyu@gmail.com>
2025-11-07 16:02:53 +0300

    Connect to the database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const PORT = process.env.PORT || 5000
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const DB = process.env.MONGODB_URI;
 
 const app = express()
 
+const conectDB = async() => {
+    try {
+        await mongoose.connect(DB);
+        console.log('DB connected!');
+        
+    } catch(err) {
+        console.error('Server Error:', err);
+    }
+};
 
 app.listen(PORT, () => {
-  console.log(`Server is running on: http://localhost:${PORT}`)
-})
+    console.log(`Server is running on: http://localhost:${PORT}`);
+    conectDB();
+});
