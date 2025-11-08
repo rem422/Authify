114c3aa94ecec7ca9e88af0600d030af583b5770
Rem Simiyu <remsimiyu@gmail.com>
2025-11-08 15:27:40 +0300

    Create sign in API route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res) => {
     const {username, email, password} = req.body;
