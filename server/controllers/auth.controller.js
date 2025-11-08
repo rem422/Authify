368321317eb10bfc3514db910aa9d66cb02005d3
Rem Simiyu <remsimiyu@gmail.com>
2025-11-08 15:15:34 +0300

    Create sign in API route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,22 +1,45 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
-export const signup = async (req, res, next) => {
+export const signup = async (req, res) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
     try{
         if(!username || !email || !password) {
-            return next(errorHandler(400, 'Please fill all the fields'));
+            return res.status(400).json({message: "Please fill all the fields"});
         }
 
         const userExists = await User.findOne({username, email});
         if(userExists) {
-            return next(errorHandler(400, 'User already exists'));
+            return res.status(400).json({message: "User already exists"});
         }
         await User.create({username, email, password: hashedPassword});
         res.status(201).json({message: "User created successfully"});
+    } catch(err) {
+        res.status(500).json({message: "Server Error", err});
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try{
+        const validUser = await User.findOne({ email });
+
+        if(!validUser) return next(errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if(!validUser || !validPassword) {
+            return next(errorHandler(401, "Invalid credentials"));
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        res
+        .cookie("access_token", token, {httpOnly: true})
+        .status(200)
+        .json(rest);
     } catch(err) {
         next(err);
     }
