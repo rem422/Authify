e27d824b0656ec79e75312eca0ac3d9e152567d1
Rem Simiyu <remsimiyu@gmail.com>
2025-11-09 18:52:33 +0300

    Create and add OAuth component

--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -1,39 +1,44 @@
-import React from 'react'
 import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
-import { app } from '../firebase.js';
+import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
-const Oauth = () => {
-    const dispatch = useDispatch();
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-    const handleGoogleClick = async() => {
-        try {
-            const provider = new GoogleAuthProvider();
-            const auth = getAuth(app);
-            const result = await signInWithPopup(auth, provider);
-            const res = await fetch('/api/auth/google', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    name: result.user.displayName,
-                    email: result.user.email,
-                    avatar: result.user.photoURL,
-                }),
-            });
-            const data = await res.json();
-            dispatch(signInSuccess(data));
-        } catch (error) {
-            console.log('Could not login with google', error);
-        }
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('could not login with google', error);
     }
-
-
+  };
   return (
-    <button type='button' onClick={handleGoogleClick} className='bg-red-700 text-white rounded-lg p-3 uppercase hover: opacity-95 cursor-pointer'>Continue with google</button>
-  )
+    <button
+      type='button'
+      onClick={handleGoogleClick}
+      className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
+    >
+      Continue with google
+    </button>
+  );
 }
-
-export default Oauth
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,43 +1,92 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import { errorHandler } from '../utils/error.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    try {
-        await newUser.save();
-    res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-        next(error);
-    }
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signin = async (req, res, next) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    try{
-        const validUser = await User.findOne({ email });
+  try {
+    const validUser = await User.findOne({ email });
 
-        if(!validUser) return next(errorHandler(404, "User not found"));
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
 
-        if(!validUser || !validPassword) {
-            return next(errorHandler(401, "Invalid credentials"));
-        }
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-        const {password: hashedPassword, ...rest} = validUser._doc;
-        res
+    if (!validUser || !validPassword) {
+      return next(errorHandler(401, "Invalid credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        expires: expiryDate,
+      })
+      .status(200)
+      .json(rest);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
         .cookie("access_token", token, {
-            httpOnly: true, 
-            maxAge: 24 * 60 * 60 * 1000, 
-            expires: new Date(Date.now() + 24 * 60 * 60 * 1000)
+          httpOnly: true,
+          expires: expiryDate,
         })
         .status(200)
         .json(rest);
-    } catch(err) {
-        next(err);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-8),
+        email: req.body.email,
+        password: hashedPassword,
+        profilePicture: req.body.photo,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const { password: hashedPassword2, ...rest } = newUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          expires: expiryDate,
+        })
+        .status(200)
+        .json(rest);
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signout = (req, res) => {
+  res.clearCookie("access_token").status(200).json("Signout success!");
+};
