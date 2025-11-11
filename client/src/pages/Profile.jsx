342197f560b8e7ae8957004fa093a1579a8acf46
Rem Simiyu <remsimiyu@gmail.com>
2025-11-11 22:08:06 +0300

    Complete image upload functionality using firebase storage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { useSelector } from "react-redux"
+import { useRef } from 'react'
 
 const Profile = () => {
-  const { currentUser } = useSelector(state => state.user)
+  const { currentUser } = useSelector(state => state.user);
+  const fileRef = useRef(null);
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
+        <input type="file" ref={fileRef} hidden accept='image/*'/>
         <img 
           src={currentUser.profilePicture} 
           alt="profile"  
           className='h-24 w-24 self-center rounded-full object-cover cursor-pointer mt-2'
+          onClick={() => fileRef.current.click()}
         />
         <input defaultValue={currentUser.username} type="text"  id='username' placeholder='Username' className='bg-slate-100 rounded-lg p-3'/>
         <input defaultValue={currentUser.email} type="email"  id='email' placeholder='Email' className='bg-slate-100 rounded-lg p-3'/>
